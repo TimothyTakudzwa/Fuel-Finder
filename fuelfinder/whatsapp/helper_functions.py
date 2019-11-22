c4c2447619f43a7cd81eb6d156a9d0087a1c2778
Timothy Ngorima <timothytakudzwa@gmail.com>
2019-11-22 21:10:33 +0200

    Working on the registration handler'

--- fuelfinder/whatsapp/helper_functions.py
+++ fuelfinder/whatsapp/helper_functions.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render 
 import requests 
+from .constants import * 
 
 def send_message(phone_number, message):
     payload = {
@@ -13,6 +14,8 @@ def send_message(phone_number, message):
 
 
 def bot_action(user, message):
+    if message.lower(0) == 'menu' and user.stage != 'registration':
+        return requests_handler
     if user.stage == 'registration':
         response_message = registration_handler(user, message)
     elif user.stage == 'requesting':
@@ -21,4 +24,24 @@ def bot_action(user, message):
         response_message = ''
     return response_message
 
-def  
+def registration_handler(user, message):
+    if user.position == 1:
+        full_name = user.first_name + user.last_name
+        response_message = greetings_message.format(full_name)
+        user.position = 2
+        user.save()
+    elif user.position == 2: 
+        if message.lower() == 'yes':
+            response_message = successful_integration
+            user.stage= 'requesting'
+            user.save()
+        else: 
+            response_message = "Unfortunately you will have to contact your admin to make changes, but for the time being er eill block this account"
+            user.stage = 'blocked'
+            user.save()
+    return response_message
+
+def requests_handler(user, message): 
+    if user.position == 1: 
+        pass 
+    return ''
