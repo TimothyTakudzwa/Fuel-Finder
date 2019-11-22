4d49d8f8ff408dcc0eba5cb2005303c4ce206859
Nathan Nyabvure <nnyabvure@zbfh.co.zw>
2019-11-22 10:40:28 +0200

    Adding the template files and project setup

--- fuelfinder/fuelfinder/settings.py
+++ fuelfinder/fuelfinder/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'finder.apps.FinderConfig',
     'django.contrib.admin',
     'users.apps.UsersConfig',
     'django.contrib.auth',
