2fc51d10973e233c834a6dac0bb36b3c6f8368bb
MarlvinzW <marlvinchihota@gmail.com>
2019-11-22 17:39:19 +0200

    update

--- fuelfinder/supplier/admin.py
+++ fuelfinder/supplier/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import SupplierProfile, FuelUpdate, FuelRequest, Province
 
-# Register your models here.
+
+admin.site.site_header = "FuelFinder Admin"
+admin.site.site_title = 'Admin Portal'
+admin.site.index_title = 'FuelFinder Admin'
+
+
+admin.site.register(SupplierProfile)
+admin.site.register(FuelUpdate)
+admin.site.register(FuelRequest)
+admin.site.register(Province)
