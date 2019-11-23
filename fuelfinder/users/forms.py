d8445abdfc2160778255057ef3a1f38fe9faa096
tinomhishi <tinotendamhishi@protonmail.com>
2019-11-23 11:38:56 +0300

    This

--- fuelfinder/users/forms.py
+++ fuelfinder/users/forms.py
@@ -10,22 +10,38 @@ from supplier.models import *
 from .utils import *
 from .widgets.select_time_widget import *
 
-class UserForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'email')
 
-class ProfileForm(forms.Form):
-    first_name = forms.CharField(label="First Name", required=True, max_length=100)
-    last_name = forms.CharField(label="Last Name", required=True, max_length=100)
-    email = forms.EmailField(required=False)
-    user = forms.ModelChoiceField(empty_label="-----", required=True,
-                    queryset=User.objects.all(),
-                    label='Pick A User',)
-    supplier_contact = forms.ModelChoiceField(empty_label="-----", required=True,
-                    queryset=SupplierProfile.objects.all(),
-                    label='Pick A Supplier',)                
+def validate_user_email(value):
+    safe = True
+    if '@' in value:
+        if User.objects.filter(email=value.strip()).exists():
+            safe = False
+    if not safe:
+        raise ValidationError('%(value)s is already registered',
+            params={'value': value},)
 
+class SupplierContactForm(forms.Form):
+    first_name = forms.CharField(label='First Name(s)', required=True,
+                                max_length=30)
+    last_name = forms.CharField(label='Last Name(s)', required=True,
+                                 max_length=30)
+    email = forms.EmailField(required=True, max_length=100,
+                            validators=[validate_user_email])
+    cellphone = forms.CharField(label='Cellphone number', required=True,
+                                 max_length=100)
+    telephone = forms.CharField(label='Telephone number', max_length=100,
+                                required=False)
+    password = forms.CharField(label='Password', required=True, max_length=100,
+                             widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password Confirm', required=True,
+                             widget=forms.PasswordInput, max_length=100)
+
+    def clean(self):
+        cleaned_data = super(OrganisationUserForm, self).clean()
+        password = cleaned_data.get("password")
+        password2 = cleaned_data.get("password2")
+        if password != password2:
+            raise forms.ValidationError("The passwords do not match!")
 class ActionForm(forms.Form):
     pass                    
 
--- fuelfinder/users/views.py
+++ fuelfinder/users/views.py
@@ -10,43 +10,45 @@ from datetime import datetime
 def index(request):
     return render(request, 'users/index.html')
 
+
+def suppliers_list(request):
+    suppliers = SupplierProfile.objects.all()
+    return render(request, 'users/suppliers_list.html', {'suppliers': suppliers})
+
 # Begining Of Supplier Management
 
-def supplier_user_create(request):
-     
+def supplier_user_create(request, sid):
+    supplier = get_object_or_404(SupplierProfile, id=sid) 
     if request.method == 'POST':
-        form = ProfileForm(request.POST)
+        user_count = SupplierContact.objects.filter(supplier_profile=supplier).count()
+        if user_count > 50:
+            raise Http404("Organisations has 50 users, delete some ")
+        form = SupplierContactForm(request.POST)
         if form.is_valid():
-            #puser = get_object_or_404(User, username='biddingwars')
-            # puser = request.user
-            # image_name = blog_save_image_upload(request)
-            image_name = True 
-            if image_name:
-                form.save()
-                user = User.objects.create(
-                    first_name=form.first,name,
-                    last_name=form.last_name,
-                    email = form.email)
-                user.save()    
-                contact = SupplierContact(
-                    user = user,
-                    supplier_profile = form.supplier_profile,
-                )
-                contact.save()
-                messages.success(request, _('Your profile was successfully updated!'))
-                return redirect('user:index')
-            else:
-                msg = "There was an error uploading the image"
-                messages.error(request, msg)    
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = User.objects.create_user(email, email, password)
+            user.last_name = form.cleaned_data['last_name']
+            user.first_name = form.cleaned_data['first_name']
+            user.save()   
+            contact = SupplierContact(user = user,
+                        cellphone=form.cleaned_data['cellphone'],
+                        telephone=form.cleaned_data['telephone'],
+                        supplier_profile = form.supplier_profile,
+                    )
+            contact.save()
+            messages.success(request, _('Your profile was successfully updated!'))
+            return redirect('user:index')
+            
         
         else:
             msg = "Error in Information Submitted"
             messages.error(request, msg)
     else:
-        form = ProfileForm()
+        form = SupplierContactForm()
 
 
-    return render (request, 'users/add_user.html', {'form': form}) 
+    return render (request, 'users/add_user.html', {'form': form, 'supplier': supplier}) 
 
 def edit_supplier(request,id):
     supplier = get_object_or_404(SupplierProfile, id=id)
